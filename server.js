4eec91d0911e1065f25d0c23f37a5de0b85c1471
GreysonR <44911101+GreysonR@users.noreply.github.com>
2024-11-03 22:54:41 -0600

    Update server code to match requirements

---
+++ server.js
@@ -0,0 +1,23 @@
+
+import http from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import fs from "node:fs";
+
+const port = 80;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+http.createServer((_req, res) => {
+	fs.readFile(join(__dirname, "public/index.html"), (err, data) => {
+		if (err) {
+			res.writeHead(404);
+			return;
+		}
+		else {
+			res.writeHead(200, { "content-type": "text/html" });
+			res.write(data);
+		}
+		res.end();
+	});
+}).listen(port);
+
+console.log("server running at http://localhost");
